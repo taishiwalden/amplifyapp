b2eb5d82c8bb16c186cb7be30fd2dfb363918e6a
Taishi <taishw@uw.edu>
2022-02-09 19:21:44 -0800

    added database and api

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -4,5 +4,23 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
+    },
+    "auth": {
+        "amplifyapp6eeb2dc4": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "CreatedSNSRole": "string"
+        }
+    },
+    "storage": {
+        "noteStorage": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
--- src/App.js
+++ src/App.js
@@ -1,54 +1,84 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import { API } from "aws-amplify";
+import { API, Storage } from "aws-amplify";
 import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
-import { listTodos } from "./graphql/queries";
-import { createTodo as createTodoMutation, deleteTodo as deleteTodoMutation } from "./graphql/mutations";
+import { listNotes } from "./graphql/queries";
+import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from "./graphql/mutations";
 
 const initialFormState = { name: "", description: "" };
 
 function App() {
-    const [Todos, setTodos] = useState([]);
+    const [Notes, setNotes] = useState([]);
     const [formData, setFormData] = useState(initialFormState);
 
     useEffect(() => {
-        fetchTodos();
+        fetchNotes();
     }, []);
 
-    async function fetchTodos() {
-        const apiData = await API.graphql({ query: listTodos });
-        setTodos(apiData.data.listTodos.items);
+    // async function fetchNotes() {
+    //     const apiData = await API.graphql({ query: listNotes });
+    //     setNotes(apiData.data.listNotes.items);
+    // }
+    async function fetchNotes() {
+        const apiData = await API.graphql({ query: listNotes });
+        const notesFromAPI = apiData.data.listNotes.items;
+        await Promise.all(
+            notesFromAPI.map(async (note) => {
+                if (note.image) {
+                    const image = await Storage.get(note.image);
+                    note.image = image;
+                }
+                return note;
+            })
+        );
+        setNotes(apiData.data.listNotes.items);
     }
 
-    async function createTodo() {
+    async function createNote() {
         if (!formData.name || !formData.description) return;
-        await API.graphql({ query: createTodoMutation, variables: { input: formData } });
-        setTodos([...Todos, formData]);
+        await API.graphql({ query: createNoteMutation, variables: { input: formData } });
+        if (formData.image) {
+            const image = await Storage.get(formData.image);
+            formData.image = image;
+        }
+        setNotes([...Notes, formData]);
         setFormData(initialFormState);
     }
 
-    async function deleteTodo({ id }) {
-        const newTodosArray = Todos.filter((Todo) => Todo.id !== id);
-        setTodos(newTodosArray);
-        await API.graphql({ query: deleteTodoMutation, variables: { input: { id } } });
+    async function deleteNote({ id }) {
+        const newNotesArray = Notes.filter((Note) => Note.id !== id);
+        setNotes(newNotesArray);
+        await API.graphql({ query: deleteNoteMutation, variables: { input: { id } } });
+    }
+
+    async function onChange(e) {
+        if (!e.target.files[0]) return;
+        const file = e.target.files[0];
+        setFormData({ ...formData, image: file.name });
+        await Storage.put(file.name, file);
+        fetchNotes();
     }
 
     return (
         <div className="App">
-            <h1>My Todos App</h1>
-            <input onChange={(e) => setFormData({ ...formData, name: e.target.value })} placeholder="Todo name" value={formData.name} />
+            <h1>My Notes App</h1>
+            <input onChange={(e) => setFormData({ ...formData, name: e.target.value })} placeholder="Note name" value={formData.name} />
             <input
                 onChange={(e) => setFormData({ ...formData, description: e.target.value })}
-                placeholder="Todo description"
+                placeholder="Note description"
                 value={formData.description}
             />
-            <button onClick={createTodo}>Create Todo</button>
+            <input type="file" onChange={onChange} />
+            <button onClick={createNote}>Create Note</button>
             <div style={{ marginBottom: 30 }}>
-                {Todos.map((Todo) => (
-                    <div key={Todo.id || Todo.name}>
-                        <h2>{Todo.name}</h2>
-                        <p>{Todo.description}</p>
-                        <button onClick={() => deleteTodo(Todo)}>Delete Todo</button>
+                {Notes.map((Note) => (
+                    <div key={Note.id || Note.name}>
+                        <h2>{Note.name}</h2>
+                        <p>{Note.description}</p>
+                        {Note.image && <img src={Note.image} style={{ width: 400 }} />}
+                        <container class="center">
+                            <button onClick={() => deleteNote(Note)}>Delete Note</button>
+                        </container>
                     </div>
                 ))}
             </div>
